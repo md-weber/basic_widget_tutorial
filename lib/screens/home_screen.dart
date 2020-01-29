937cad4212501c9838d2758e1fd78e8ca85ef314
myracledesign <weber.max.1988@live.de>
2020-01-29 18:54:03 +0100

    improved to the next level

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool loading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hello Youtube"),
       ),
-      body: Container(
-        child: Center(
-            child: Text(
-          "Hello World!",
-        )),
-      ),
+      body: loading
+          ? Container(
+              child: Center(
+                  child: Text(
+                "Hello World!",
+                style: TextStyle(fontSize: 38.0),
+              )),
+            )
+          : Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      loading = !loading;
+                    });
+                    print(loading);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Text(
+                      "Loading Toggle",
+                      style: TextStyle(fontSize: 32.0),
+                    ),
+                  ),
+                )
+              ],
+            )),
     );
   }
 }
